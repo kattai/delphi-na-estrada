3da7d06a4dbab6451d43fb817560796b6bc3348a
Brunno Mendes <bh.rodrigues@gdksoftware.com>
2025-02-10 15:17:50 -0300

    Updates

---
+++ precache-manifest.4edf80881103f7f62f6a659255b64678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5746c94d72ff4ddc34fc87860d48bc",
+    "url": "/delphi-na-estrada/index.html"
+  },
+  {
+    "revision": "33beac73fc3178c6761a",
+    "url": "/delphi-na-estrada/static/css/2.c98225b9.chunk.css"
+  },
+  {
+    "revision": "75c5a3ff53a9da943b24",
+    "url": "/delphi-na-estrada/static/css/main.cedcabe1.chunk.css"
+  },
+  {
+    "revision": "33beac73fc3178c6761a",
+    "url": "/delphi-na-estrada/static/js/2.b0e6c5b2.chunk.js"
+  },
+  {
+    "revision": "75c5a3ff53a9da943b24",
+    "url": "/delphi-na-estrada/static/js/main.cc12e624.chunk.js"
+  },
+  {
+    "revision": "f4a40b2dbf26e5b6293c",
+    "url": "/delphi-na-estrada/static/js/runtime~main.6a183f08.js"
+  }
+]);
